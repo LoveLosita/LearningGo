4948079f907839c9b1dde53ef6b2187ae1b3f106
LoveLosita <2810873701@qq.com>
2024-11-23 10:53:18 +0800

    添加新路由

--- class5/homework/lv3_project/main.go
+++ class5/homework/lv3_project/main.go
@@ -7,9 +7,13 @@ import (
 func main() {
 	h := server.Default()
 	//h.GET("/change", ChangeGood)
-	h.GET("/show", ShowCustomerInfo)             //已经验收没啥问题
+	h.GET("/showCustomer", ShowCustomerInfo)     //已经验收没啥问题
 	h.GET("/changeCustomer", ChangeCustomerInfo) //已经验收没啥问题
 	h.GET("/addCustomer", AddCustomer)           //已经验收没啥问题
 	h.GET("/deleteCustomer", DeleteCustomer)     //已经验收没啥问题
+	h.GET("/changeGood", ChangeGood)
+	h.GET("/addGood", AddGood)
+	h.GET("/deleteGood", DeleteGood)
+	h.GET("/showGood", ShowGoodInfo)
 	h.Spin()
 }
