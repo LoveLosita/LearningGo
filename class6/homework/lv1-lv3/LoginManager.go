d7ffef242ce49173857d3465c201a731d5ec85fd
LoveLosita <2810873701@qq.com>
2024-12-02 19:32:27 +0800

    添加注释，大幅度提高代码可读性

--- class6/homework/lv1-lv3/LoginManager.go
+++ class6/homework/lv1-lv3/LoginManager.go
@@ -18,12 +18,14 @@ type ChangePasswordStudent struct {
 	NewPassword string `json:"newPassword"`
 }
 
-func Login(ctx context.Context, c *app.RequestContext) {
+func Login(ctx context.Context, c *app.RequestContext) { //登录模块
+	//1.处理POST请求
 	student := UserStudent{}
-	err := c.BindJSON(&student)
+	err := c.BindJSON(&student) //从POST请求中获取学生数据，存入student结构体中
+	//2.准备开始对比用户名密码
 	name := ""
 	getPassword := ""
-	query := "SELECT name,password FROM students WHERE username=?"
+	query := "SELECT name,password FROM students WHERE username=?" //从数据库中提取用户名和密码，用于下面的比较
 	rows, err := Db.Query(query, student.Username)
 	if err != nil {
 		c.JSON(consts.StatusBadRequest, map[string]string{
@@ -31,11 +33,12 @@ func Login(ctx context.Context, c *app.RequestContext) {
 		})
 		return
 	}
-	if !rows.Next() {
+	//3.对比密码
+	if !rows.Next() { //上面使用POST请求中获取的用户名扫描未获得结果，即用户名错误
 		c.JSON(consts.StatusBadRequest, map[string]string{
 			"登录失败": "错误的用户名或密码",
 		})
-	} else {
+	} else { //找到了用户，进行下一步密码的判断
 		err = rows.Scan(&name, &getPassword)
 		if err != nil {
 			c.JSON(consts.StatusBadRequest, map[string]string{
@@ -43,11 +46,11 @@ func Login(ctx context.Context, c *app.RequestContext) {
 			})
 			return
 		}
-		if getPassword != student.Password {
+		if getPassword != student.Password { //POST请求中的密码和数据库中的密码不相等，即密码错误
 			c.JSON(consts.StatusBadRequest, map[string]string{
 				"登录失败": "错误的用户名或密码",
 			})
-		} else {
+		} else { //相等，即成功登录
 			c.JSON(consts.StatusBadRequest, map[string]string{
 				"登录成功": "欢迎" + name + "同学",
 			})
@@ -56,6 +59,7 @@ func Login(ctx context.Context, c *app.RequestContext) {
 }
 
 func ChangePassword(ctx context.Context, c *app.RequestContext) {
+	//1.从POST请求获取数据
 	student := ChangePasswordStudent{}
 	err := c.BindJSON(&student)
 	oldPassword := ""
@@ -66,6 +70,7 @@ func ChangePassword(ctx context.Context, c *app.RequestContext) {
 		})
 		return
 	}
+	//2.从数据库中提取名字和密码，用于下一步比较
 	query := "SELECT name,password FROM students WHERE username=?"
 	rows, err := Db.Query(query, student.Username)
 	if err != nil {
@@ -74,25 +79,26 @@ func ChangePassword(ctx context.Context, c *app.RequestContext) {
 		})
 		return
 	}
-	if !rows.Next() {
+	//3.开始比较
+	if !rows.Next() { //找不到用户名，说明用户名输入错误
 		c.JSON(consts.StatusBadRequest, map[string]string{
 			"error": "用户名不存在！",
 		})
 		return
-	} else {
-		err = rows.Scan(&username, &oldPassword)
+	} else { //找到了用户名，需要进一步对比旧密码是否正确
+		err = rows.Scan(&username, &oldPassword) //从数据库中提取用户名和旧密码
 		if err != nil {
 			c.JSON(consts.StatusBadRequest, map[string]string{
 				"error": err.Error(),
 			})
 			return
 		}
-		if student.OldPassword != oldPassword {
+		if student.OldPassword != oldPassword { //如果POST请求中的旧密码和数据库中的密码不一样，即旧密码错误
 			c.JSON(consts.StatusBadRequest, map[string]string{
 				"error": "旧密码错误，修改失败！",
 			})
 			return
-		} else {
+		} else { //密码正确，开始执行修改的流程
 			query = "UPDATE students SET password=? WHERE username=?"
 			result, err := Db.Exec(query, student.NewPassword, student.Username)
 			if err != nil {
--- class6/homework/lv1-lv3/stuManagement.go
+++ class6/homework/lv1-lv3/stuManagement.go
@@ -17,7 +17,8 @@ type Student struct {
 	Password string `json:"password"`
 }
 
-func DeleteStudents(ctx context.Context, c *app.RequestContext) {
+func DeleteStudents(ctx context.Context, c *app.RequestContext) { //删除学生模块
+	//1.从GET请求中获取数据
 	id := c.Query("id")
 	intID, err := strconv.ParseInt(id, 10, 0)
 	if err != nil {
@@ -27,7 +28,7 @@ func DeleteStudents(ctx context.Context, c *app.RequestContext) {
 		return
 	}
 	query := "DELETE FROM students WHERE id = ?"
-	// 执行删除操作
+	// 2.执行删除操作
 	result, err := Db.Exec(query, int(intID))
 	if err != nil {
 		c.JSON(consts.StatusBadRequest, map[string]string{
@@ -35,7 +36,7 @@ func DeleteStudents(ctx context.Context, c *app.RequestContext) {
 		})
 		return
 	}
-	// 检查删除的行数
+	// 3.检查删除的行数
 	rowsAffected, err := result.RowsAffected()
 	if err != nil {
 		c.JSON(consts.StatusBadRequest, map[string]string{
@@ -43,7 +44,7 @@ func DeleteStudents(ctx context.Context, c *app.RequestContext) {
 		})
 		return
 	}
-
+	//4.通过删除行数倒推是否删除，如果为0就是没删除，相当于没找到
 	if rowsAffected == 0 {
 		c.JSON(consts.StatusBadRequest, map[string]string{
 			"error": "没找到学生",
@@ -55,7 +56,8 @@ func DeleteStudents(ctx context.Context, c *app.RequestContext) {
 	})
 }
 
-func AddStudents(ctx context.Context, c *app.RequestContext) {
+func AddStudents(ctx context.Context, c *app.RequestContext) { //添加学生模块
+	//1.从POST请求中获取信息
 	studentInfo := Student{}
 	err := c.BindJSON(&studentInfo) //从Json请求中获取信息
 	if err != nil {
@@ -64,6 +66,7 @@ func AddStudents(ctx context.Context, c *app.RequestContext) {
 		})
 		return
 	}
+	//2.执行数据库操作，准备插入新学生
 	query := ""
 	query = "INSERT INTO students (name, age, gender, class,username,password) VALUES (?, ?, ?, ?,?,?)"
 	_, err = Db.Exec(query, studentInfo.Name, studentInfo.Age, studentInfo.Gender, studentInfo.Class, studentInfo.Username, studentInfo.Password)
@@ -78,14 +81,15 @@ func AddStudents(ctx context.Context, c *app.RequestContext) {
 	})
 }
 
-func ChangeStudentInfo(ctx context.Context, c *app.RequestContext) {
+func ChangeStudentInfo(ctx context.Context, c *app.RequestContext) { //改变学生信息模块
+	//1.从POST请求中获取信息
 	newStudentInfo := Student{}
 	err := c.BindJSON(&newStudentInfo)
 	targetID := c.Query("id")
 	intTargetID, err := strconv.ParseInt(targetID, 10, 0)
 	student := Student{}
 	rows, err := Db.Query("SELECT id ,name, age, gender, class,username,password FROM students WHERE id = ?", int(intTargetID))
-	if newStudentInfo.Id != 0 {
+	if newStudentInfo.Id != 0 { //此处为了防止更改id，导致自增炸
 		c.JSON(consts.StatusBadRequest, map[string]string{
 			"error": "不允许更改id！",
 		})
@@ -97,7 +101,7 @@ func ChangeStudentInfo(ctx context.Context, c *app.RequestContext) {
 		})
 		return
 	}
-	if newStudentInfo.Username != "" {
+	if newStudentInfo.Username != "" { //判断是否在新信息中填写了名字，即是否想改名字
 		ifSameUsername, err := Db.Query("SELECT id ,name, age, gender, class,username,password FROM students WHERE username = ?", newStudentInfo.Username)
 		if err != nil {
 			c.JSON(consts.StatusBadRequest, map[string]string{
@@ -105,14 +109,13 @@ func ChangeStudentInfo(ctx context.Context, c *app.RequestContext) {
 			})
 			return
 		}
-		if ifSameUsername.Next() {
+		if ifSameUsername.Next() { //如果通过要改的名字能找到学生，说明用户名已存在
 			c.JSON(consts.StatusBadRequest, map[string]string{
 				"error": "用户名已存在！",
 			})
 			return
 		}
 	}
-	// 这里加入 rows.Next()
 	if rows.Next() { // 确保至少有一行返回
 		err := rows.Scan(&student.Id, &student.Name, &student.Age, &student.Gender, &student.Class, &student.Username, &student.Password)
 		if err != nil {
@@ -128,6 +131,7 @@ func ChangeStudentInfo(ctx context.Context, c *app.RequestContext) {
 		})
 		return
 	}
+	//下面是一个选择性填写的设计，只需要填写想更改的信息就行了，不想改可以直接不用填写
 	if newStudentInfo.Name != "" {
 		student.Name = newStudentInfo.Name
 	}
@@ -139,6 +143,7 @@ func ChangeStudentInfo(ctx context.Context, c *app.RequestContext) {
 	}
 	if newStudentInfo.Gender != "" && (newStudentInfo.Gender == "男" || newStudentInfo.Gender == "女") {
 		student.Gender = newStudentInfo.Gender
+		//这里是判断性别是否正确，把错误扼杀在写入数据库之前
 	} else if newStudentInfo.Gender != "" && !(newStudentInfo.Gender == "男" || newStudentInfo.Gender == "女") {
 		c.JSON(consts.StatusBadRequest, map[string]string{
 			"error": "性别错误！",
@@ -150,6 +155,8 @@ func ChangeStudentInfo(ctx context.Context, c *app.RequestContext) {
 	if newStudentInfo.Age != 0 {
 		student.Age = newStudentInfo.Age
 	}
+	//选择性填写设计模块至此结束
+	//下面开始更新数据库信息
 	query := "UPDATE students SET name=?, age=?,gender=?,class=?,username=?,password=? WHERE id=?"
 	result, err := Db.Exec(query, student.Name, student.Age, student.Gender, student.Class, student.Username, student.Password, intTargetID)
 	if err != nil {
@@ -173,7 +180,8 @@ func ChangeStudentInfo(ctx context.Context, c *app.RequestContext) {
 	})
 }
 
-func FindStudent(ctx context.Context, c *app.RequestContext) {
+func FindStudent(ctx context.Context, c *app.RequestContext) { //查询学生的模块
+	//1.从url中获取想要查找学生的id
 	id := c.Query("id")
 	rows, err := Db.Query("SELECT id ,name, age, gender, class,username,password FROM students WHERE id = ?", id)
 	if err != nil {
@@ -182,10 +190,8 @@ func FindStudent(ctx context.Context, c *app.RequestContext) {
 		})
 		return
 	}
-	student := Student{}
-	// 使用 rows.Next() 遍历查询结果
-	if rows.Next() {
-		// 只有在 rows.Next() 返回 true 时才调用 rows.Scan
+	student := Student{} //准备结构体用于写入信息并呈现
+	if rows.Next() {     //找到学生了
 		err := rows.Scan(&student.Id, &student.Name, &student.Age, &student.Gender, &student.Class, &student.Username, &student.Password)
 		if err != nil {
 			c.JSON(consts.StatusBadRequest, map[string]string{
@@ -194,7 +200,7 @@ func FindStudent(ctx context.Context, c *app.RequestContext) {
 			return
 		}
 	} else {
-		// 如果没有找到数据，返回相应的错误信息
+		// 如果没有找到学生
 		c.JSON(consts.StatusNotFound, map[string]string{
 			"error": "没有找到指定的学生",
 		})
